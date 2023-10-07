864866b931260ace050c4fe9120a3c67ccf998fa
Shafi Masoumi <masoumishafi@gmail.com>
2023-10-07 14:11:55 -0700

    home page updates

--- src/Componets/Home.jsx
+++ src/Componets/Home.jsx
@@ -5,8 +5,8 @@ const Home = () => {
     <hearder>
       <div className='homePage'>
         <hi className='homeH1'>We provide,<br />low price</hi>
-        <div>
-          <p className='homeP'>some more information about the business or serivce will come here about the business or serive will come here
+        <div >
+          <p className='homeP'>some more information about the business or serive will come here about the business or serive will come here
             about the business or serive will come here </p>
         </div>
       </div>
