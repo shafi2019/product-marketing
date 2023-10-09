47217f4da172e2adbe92cd9356ed30f25d13c09f
Shafi Masoumi <masoumishafi@gmail.com>
2023-10-09 10:12:07 -0700

    loading state added

--- src/Componets/Product/CardButton.jsx
+++ src/Componets/Product/CardButton.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, message } from 'antd'
 import { addToCart } from '../../API/Api'
 
 function CardButton({ item }) {
+    const [loading, setLoading] = useState(false)
     const addProductToCart = () => {
+        setLoading(true)
         addToCart(item.id).then(res => {
             message.success(`${item.title} has been added to the Cart`)
+            setLoading(false)
         })
     }
     return (
@@ -13,10 +16,12 @@ function CardButton({ item }) {
             type="link"
             onClick={() => {
                 addProductToCart();
-            }}>
-                    Add to cart
+            }}
+            loading={loading}
+        >
+            Add to cart
         </Button >
-  )
-            }
+    )
+}
 
 export default CardButton
