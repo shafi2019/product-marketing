a3c9e12385675c32de4241af59d19f402e3f5c6d
Shafi Masoumi <masoumishafi@gmail.com>
2023-10-09 15:21:18 -0700

    checkout form created and forms validation added

--- src/Componets/Cart.jsx
+++ src/Componets/Cart.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import { Badge, Drawer, InputNumber, Table } from 'antd'
+import { Badge, Button, Drawer, Form, Input, InputNumber, Table} from 'antd'
 import { ShoppingCartOutlined } from "@ant-design/icons"
 import { getCart } from '../API/Api';
 
 function Cart() {
+
     const [cartDrawerOpen, setCartDrawerOpen] = useState(false);
     const [cartItems, setCartItems] = useState([])
+    const [checkoutDrawerOpen, setCheckoutDrawerOpen] = useState(false)
+
     useEffect(() => {
         getCart().then(res => {
             setCartItems(res.products)
@@ -73,6 +76,48 @@ function Cart() {
                         return <span>Total: {total}</span>
                     }}
                 />
+               <Button 
+               type="primary"
+               onClick={() => {
+                setCheckoutDrawerOpen(true)
+               }}
+               >
+                Checkout
+            </Button>
+            </Drawer>
+            <Drawer open={checkoutDrawerOpen} onClose={() => {
+                setCheckoutDrawerOpen(false)
+            }}
+            title="Confirm Your Order"
+            >
+                <Form>
+                    <Form.Item rules={[
+                        {
+                            required:true,
+                            message:'Please enter your full name',
+                        }
+                    ]} label='Full Name' name='full_name'>
+                        <Input placeholder='Enter your full name'/>
+                    </Form.Item>
+                    <Form.Item rules={[
+                        {
+                            required:true,
+                            message:'Please entea valid email address',
+                            type:'email'
+                        }
+                    ]} label='Email Address' name='email_address'>
+                        <Input placeholder='email address'/>
+                    </Form.Item>
+                    <Form.Item rules={[
+                        {
+                            required:true,
+                            message:'Please enter your address',
+                        }
+                    ]} label='Home Address' name='home_address'>
+                        <Input placeholder='enter your address'/>
+                    </Form.Item> 
+                    <Button type='primary'>Confirm your order</Button>
+                </Form>
 
             </Drawer>
         </div>
