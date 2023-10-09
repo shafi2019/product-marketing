03d28336a90c6745e0613c59a20f4ed7d90f87db
Shafi Masoumi <masoumishafi@gmail.com>
2023-10-09 12:30:40 -0700

    Cart Drawer added

--- src/Componets/Cart.jsx
+++ src/Componets/Cart.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import { Badge } from 'antd'
+import React, { useState } from 'react'
+import { Badge, Drawer } from 'antd'
 import { ShoppingCartOutlined } from "@ant-design/icons"
 
 function Cart() {
+    const [cartDrawerOpen, setCartDrawerOpen] = useState(false);
     return (
         <div>
-            <Badge count="7" className="shoppingCartIcon">
+            <Badge 
+            onClick={() => {
+                setCartDrawerOpen(true)
+            }}
+            count="7" 
+            className="shoppingCartIcon">
                 <ShoppingCartOutlined />
             </Badge>
+            <Drawer 
+              open={cartDrawerOpen}
+              onClose={() => {
+                setCartDrawerOpen(false)
+              }}
+            >             
+            </Drawer>
         </div>
     )
 }
